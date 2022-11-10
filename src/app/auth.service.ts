471160be0c5280de2536d1b53780901a0d9272e4
Gheorghe Bulicanu <gicu@live.com>
2022-11-10 13:58:45 +0200

    Redirect user after login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { GoogleAuthProvider, User } from 'firebase/auth';
 import { Injectable } from '@angular/core';
 import { Auth, authState, signInWithRedirect, signOut } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,13 @@ import { Observable } from 'rxjs';
 export class AuthService {
   user$: Observable<User | null>;
 
-  constructor(private afa: Auth) {
+  constructor(private afa: Auth, private route: ActivatedRoute) {
     this.user$ = authState(afa);
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     console.log('login:start');
     signInWithRedirect(this.afa, new GoogleAuthProvider())
       .then(c => console.log('login:end', c));
