bdc70a35980147b3a0fb006b27718747a74f0ae1
Gheorghe Bulicanu <gicu@live.com>
2022-12-14 12:46:09 +0200

    Feeding the Data Table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ProductService } from '../../product.service';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
 import { Subscription } from 'rxjs';
+
+import { ProductService } from '../../product.service';
+import { Product } from '../../models/product';
+import { MatSort, Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,26 +15,53 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products!: { [key: string]: { title: string, price: number } };
-  filteredProducts!: { [key: string]: { title: string, price: number } };
   subscription: Subscription;
+  productsDataSource!: MatTableDataSource<Product>;
+  displayedColumns: string[] = [ 'title', 'price', 'actions' ];
 
-  constructor(products: ProductService) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    if (this.productsDataSource) {
+      this.productsDataSource.paginator = this.paginator;
+      this.productsDataSource.sort = this.sort;
+    }
+  }
+
+
+  constructor(products: ProductService, private liveAnnouncer: LiveAnnouncer) {
     this.subscription = products.getAll()
-      .subscribe(allProducts =>
-        this.filteredProducts = this.products = allProducts);
+      .subscribe(allProducts => {
+        this.productsDataSource = new MatTableDataSource<Product>(
+            Object.entries(allProducts).map(([key, product]) => ({key, ...product}))
+          );
+
+        this.productsDataSource.paginator = this.paginator;
+        this.productsDataSource.sort = this.sort;
+
+        this.productsDataSource.filterPredicate = (record, filter)  => {
+          return record.title.toLowerCase().includes(filter.toLowerCase());
+        }
+      });
+  }
+
+  // Announce the change in sort state for assistive technology.
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this.liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
   filter(query: string) {
     if (!query)  {
-      this.filteredProducts = this.products;
+      this.productsDataSource.filter = '';
       return;
     }
 
-    this.filteredProducts = Object.fromEntries(
-      Object.entries(this.products)
-        .filter(([, value]) =>
-          value.title.toLowerCase().includes(query.toLowerCase())));
+    this.productsDataSource.filter = query.trim().toLowerCase();
   }
 
   ngOnDestroy(): void {
