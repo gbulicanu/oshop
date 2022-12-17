b67295b08afa364688a06722cdb39608b72dc73c
Gheorghe Bulicanu <gicu@live.com>
2022-12-17 14:07:39 +0200

    Display All the Products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private products: ProductService) {
+    this.products$ = products.getAll();
   }
 
 }
