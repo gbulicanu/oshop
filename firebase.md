4991ca8604aace9003df4bed587c661a7974c8c0
Gheorghe Bulicanu <gicu@live.com>
2023-01-11 10:52:45 +0200

    Firebase re-target

---
+++ firebase.md
@@ -0,0 +1,7 @@
+# Firebase
+
+`+  Created service account github-action-564202563 with Firebase Hosting admin permissions.`
+`+  Uploaded service account JSON to GitHub as secret FIREBASE_SERVICE_ACCOUNT_OSHOP_374407.`
+`i  You can manage your secrets at https://github.com/gbulicanu/oshop/settings/secrets.`
+
+`? Set up the workflow to run a build script before every deploy? (y/N)`
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDhCnaPyMehECcml947uF9SoyC5u2fxZrY",
-    authDomain: "oshop-df240.firebaseapp.com",
-    projectId: "oshop-df240",
-    storageBucket: "oshop-df240.appspot.com",
+    apiKey: "AIzaSyCDqoKCYP4Is3hkj--81veAbHY6aj26MnM",
+    authDomain: "oshop-374407.firebaseapp.com",
+    projectId: "oshop-374407",
+    storageBucket: "oshop-374407.appspot.com",
     messagingSenderId: "357812111782",
     appId: "1:357812111782:web:6470f8842ce5a89e362767",
     measurementId: "G-1ZGWY26L39",
-    databaseURL: 'https://oshop-df240-default-rtdb.firebaseio.com/'
+    databaseURL: 'https://oshop-374407-default-rtdb.europe-west1.firebasedatabase.app/'
   }
 };
